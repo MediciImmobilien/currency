ccbfac8702fbd4af791eef46391bc4d680bd2a7b
MediciImmobilien <lutzvogelsang@Lutzs-MacBook-Pro-6.local>
2017-07-27 12:38:54 +0200

    update

--- lib/currency.ex
+++ lib/currency.ex
@@ -1,15 +1,33 @@
 defmodule Currency do
-	def exchange_rate(currency_from, currency_to) do
-		{:ok, %{body: body}} = HTTPoison.get("http://api.fixer.io/latest?base=USD&symbols=EUR")
-		%{"rates" => %{"EUR" => exchange_rate}} = body |> Poison.decode!
-		 exchange_rate / 100 
+	use GenServer
+	
+	def start(_type, _args), do: {:ok, pid} = GenServer.start_link(Currency, {"USD", "EUR"})
+
+	def from_usd_to_eur, do: GenServer.call(pid, %{amount: 100, currency: :USD})
+
+	def init({from, to}), do: {:ok, update_rate(from, to)}
+	
+	def url(from, to), do: "http://api.fixer.io/latest?base=#{from}&symbols=#{to}"
+	
+	def update_rate(from, to), do: {url(from, to) |> request, from, to , Date.utc_today}
+	
+	def request(url), do: url |> HTTPoison.get! |> handle_response 
+	
+	def handle_response(%{body: body}), do: body |> Poison.decode! |> Map.get("rates") |> Map.get("EUR")
+
+	def handle_call(%{amount: amount, currency: :USD}, _from, state) do
+		new_state = state |> state_up_to_date?
+		{:reply, amount |> exchange(new_state) , new_state}	
 	end
 	
-	def from_usd_to_eur(%{amount: amount, currency: :USD}) do
-		{:ok,new_amount} = amount * exchange_rate("USD", "EUR")
-		|> Float.round(2)
-		|> Money.parse(:EUR)
-		new_amount
+	def state_up_to_date?({rate,from,to, date}), do: Date.compare(date, Date.utc_today)|> state_up_to_date?({rate,from,to, date})
 		
+	def state_up_to_date?(:eq,{rate,from,to, date}), do: {rate,from,to, date}
+	
+	def state_up_to_date?(:lt, {rate,from,to, date}), do: update_rate(from, to)
+	
+	def exchange(amount,{rate,_,_,_}) do 
+  		{:ok,new_amount} = amount * (rate/100)|> Float.round(2)|> Money.parse(:EUR)
+		new_amount
 	end
 end
