4f0903a7bd8a2604d665c0f7cb39c80b1344b576
MediciImmobilien <lutzvogelsang@Lutzs-MacBook-Pro-6.local>
2017-11-06 16:54:13 +0100

    fixes

--- mix.exs
+++ mix.exs
@@ -1,35 +1,22 @@
 defmodule Currency.Mixfile do
-  use Mix.Project
+  	use Mix.Project
 
-  def project do
-    [app: :currency,
-     version: "0.2.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
-  end
+  	def project do
+    	[app: :currency,
+     	version: "0.3.0",
+     	elixir: "~> 1.4",
+     	build_embedded: Mix.env == :prod,
+     	start_permanent: Mix.env == :prod,
+     	deps: deps()]
+  	end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
-  def application do
-    # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger, :httpoison, :money, :poison]]
-  end
+	def application do
+    	[extra_applications: [:logger, :httpoison, :money, :poison],mod: {Currency, []}]
+ 	end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
-  defp deps do
-    [ {:httpoison, "~> 0.11.0"},
+	defp deps do
+    	[{:httpoison, "~> 0.13.0"},
 		{:money, "~> 1.2"},
-	 {:poison, "~> 3.1",override: :true}]
-  end
+	 	{:poison, "~> 3.1",override: :true}]
+  	end
 end
